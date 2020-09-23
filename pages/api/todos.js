3967e4a843e5b23a43817fae8e633cae0562a12e
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-24 00:30:00 +0200

    feat: Add basic method handler

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,16 +1,29 @@
 import query from "../../lib/db";
 
 const todos = async (req, res) => {
-  const todos = await query`select * from todos;`;
+  switch (req.method) {
+    case "GET": {
+      const todos = await query`select * from todos order by insertDate;`;
 
-  res.statusCode = 200;
-  res.json(
-    todos.map((t) => ({
-      id: t.id,
-      title: t.title,
-      body: t.body,
-    }))
-  );
+      res.statusCode = 200;
+      res.json(
+        todos.map((t) => ({
+          id: t.id,
+          title: t.title,
+          body: t.body,
+        }))
+      );
+
+      return;
+    }
+
+    default: {
+      res.statusCode = 405;
+      res.end("method not allowed");
+
+      return;
+    }
+  }
 };
 
 export default todos;
