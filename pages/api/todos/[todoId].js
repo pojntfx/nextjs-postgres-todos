524959b973550292b1ded8a7378651978ff52c09
Felix Pojtinger <felix@pojtinger.com>
2020-09-24 00:52:08 +0200

    feat: Add DELETE handler to /todos/:id

--- pages/api/todos/[todoId].js
+++ pages/api/todos/[todoId].js
@@ -24,6 +24,30 @@ const todo = async (req, res) => {
       return;
     }
 
+    case "DELETE": {
+      const [
+        fetchedTodo,
+      ] = await query`select * from todos where id = ${req.query.todoId};`;
+
+      if (!fetchedTodo) {
+        res.statusCode = 404;
+        res.end("todo not found");
+
+        return;
+      }
+
+      await query`delete from todos where id = ${req.query.todoId};`;
+
+      res.statusCode = 200;
+      res.json({
+        id: fetchedTodo.id,
+        title: fetchedTodo.title,
+        body: fetchedTodo.body,
+      });
+
+      return;
+    }
+
     default: {
       res.statusCode = 405;
       res.end("method not allowed");
