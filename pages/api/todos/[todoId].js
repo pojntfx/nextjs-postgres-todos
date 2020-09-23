19a39640891379739b17f3a5b9f49f871eb39c7c
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-24 00:49:01 +0200

    feat: Add GET handler to /todos/:id

---
+++ pages/api/todos/[todoId].js
@@ -0,0 +1,36 @@
+import query from "../../../lib/db";
+
+const todo = async (req, res) => {
+  switch (req.method) {
+    case "GET": {
+      const [
+        fetchedTodo,
+      ] = await query`select * from todos where id = ${req.query.todoId};`;
+
+      if (!fetchedTodo) {
+        res.statusCode = 404;
+        res.end("todo not found");
+
+        return;
+      }
+
+      res.statusCode = 200;
+      res.json({
+        id: fetchedTodo.id,
+        title: fetchedTodo.title,
+        body: fetchedTodo.body,
+      });
+
+      return;
+    }
+
+    default: {
+      res.statusCode = 405;
+      res.end("method not allowed");
+
+      return;
+    }
+  }
+};
+
+export default todo;
